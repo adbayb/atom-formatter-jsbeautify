80878b7c82b14e9686ea3f06ed5485180352f9db
ayoubdev <ayoub.development@gmail.com>
2016-05-01 23:41:26 +0200

    => Removed .jsbeautifyrc => Removed lib/formatter folder => Moved lib/utility.js to lib/api/index.js => Added test files to test beautification processes => lib/beautifier/error.js: Added name, message getters => lib/beautifier/error.js: Added toString method => lib/beautifier/index.js: Added css and html beautifier methods => lib/beautifier/index.js: Added beautifier methods without configuration (jsDefault(), cssDefault(), htmlDefault()) => lib/beautifier/index.js: Added resetConfig() method => lib/init.js: Updated catch error instructions to allow beautification without configurations => lib/init.js: Added selectors matches to manage types beautification => lib/init.js: Injecting corresponding beautifier methods accordingly to selectors => lib/init.js: Transform FormatterJSBeautify object to ES2015 class (arbitrary named AtomPlugin class)

---
+++ test/index.js
@@ -0,0 +1,7 @@
+function(example) {
+	if(example) {
+		console.log("I'm a beautfied content", example);
+		return true;
+	} else
+		return false;
+}
---
+++ test/index.jsx
@@ -0,0 +1,14 @@
+class Example {
+	render() {
+		<h1>
+			JSX Example
+		</h1>;
+	}
+
+	test() {
+		if(true)
+			return true;
+
+		return false;
+	}
+}
